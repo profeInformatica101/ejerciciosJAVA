4a42269b8bcded4587676185a1e0db9f25707f86
Pablo <jdommay863@g.educaand.es>
2026-01-22 20:01:47 +0100

    Ejercicio Casa y Bombilla

---
+++ Ejercicios/ActividadesU7/CasaAPP/src/main/java/com/casa/Bombilla.java
@@ -0,0 +1,14 @@
+package com.casa;
+
+public class Bombilla {
+	private boolean estado;
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+	
+}
---
+++ Ejercicios/ActividadesU7/CasaAPP/src/main/java/com/casa/Casa.java
@@ -0,0 +1,6 @@
+package com.casa;
+
+public class Casa {
+	private Bombilla[] bombillas;
+	private Boolean fusible;
+}
